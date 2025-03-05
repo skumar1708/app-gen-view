d4378b73c0b4c74c7ed52e5aafb35f30e82177c8
skumar1708 <itbhu.sk@gmail.com>
2025-03-05 15:21:29 +0530

    updated next config json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-            // source: '/socket.io/:path*',  // Match Socket.IO requests
-            // destination: 'http://localhost:5000/socket.io/:path*', // Your server URL
-            // destination: `https://app-generator-backend.vercel.app/socket.io/:path`
-            },
-        ];
-        }
+   
     };
     
     export default nextConfig;
